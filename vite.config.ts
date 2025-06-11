47612949072893868a756390fee68202bc8630e5
Dmitry <dmitrymumrenkov@gmail.com>
2025-06-11 22:37:55 +0300

    test

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,12 @@ export default defineConfig({
   preview: {
     port: 3000
   },
-  base: '/'
+  base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
